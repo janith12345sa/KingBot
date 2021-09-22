a7fbb6fd4aaecddad5449d9c035b7c9396e2d544
Janith sadanuwan <89557325+janith12345sa@users.noreply.github.com>
2021-09-22 12:05:32 +0530

    😈kingbot😈

---
+++ kingbot/index.js
@@ -0,0 +1,16 @@
+/* Coded By Ravindu Manoj.
+
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+Whats bot - Ravindu Manoj
+*/
+
+module.exports = {
+    Base: require('./Base'),
+    Message: require('./Message'),
+    StringSession: require('./StringSession'),
+    ReplyMessage: require('./ReplyMessage'),
+    Image: require('./Image'),
+    Video: require('./Video')
+};

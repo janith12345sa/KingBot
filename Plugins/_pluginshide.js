0206e6ca62bfea743735b2948299b477d120788f
Janith sadanuwan <89557325+janith12345sa@users.noreply.github.com>
2021-09-22 11:57:21 +0530

    😈kingbot😈

---
+++ Plugins/_pluginshide.js
@@ -0,0 +1,14 @@
+const PL = require("plugins/hideplugins.py")
+
+     run="const.PL"
+
+#bad word kick
+#grouplink kick
+#text image
+#more powerful item 😁
+#new text to imsge
+#emoji to txt
+#bad word kick out
+#anti group link
+#spam kick Added
+#Group Crash Added

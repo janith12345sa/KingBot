ba873b7ebc910b0a1b96a57136c3bbcaab710106
Janith sadanuwan <89557325+janith12345sa@users.noreply.github.com>
2021-09-28 14:46:20 +0530

    😈kingbot😈 V  3.0  ♻️

--- qr.js
+++ qr.js
@@ -1,18 +1,18 @@
-/* Codded by @Ravindu Manoj
+/* Codded by @Janithsadanuwan 
 
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-Whats bot - Ravindu Manoj
+Whats bot - Janithsadanuwan 
 */
 
 const chalk = require('chalk');
 const {WAConnection, MessageOptions, MessageType} = require('@adiwajshing/baileys');
-const {StringSession} = require('./sewqueen/');
+const {StringSession} = require('./Kingbot/');
 const fs = require('fs');
 
-async function sewQueen () {
+async function Kingbot () {
     const conn = new WAConnection();
     const Session = new StringSession();  
     conn.version = [2, 2126, 14]
@@ -20,8 +20,8 @@ async function sewQueen () {
     conn.regenerateQRIntervalMs = 50000;
     
     conn.on('connecting', async () => {
-        console.log(`${chalk.green.bold('Queen')}${chalk.blue.bold('Sew')}
-${chalk.white.italic('QueenSewString')}
+        console.log(`${chalk.green.bold('King')}${chalk.blue.bold('bot')}
+${chalk.white.italic('KingbotString')}
 
 ${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please Wait.')}`);
     });
@@ -30,7 +30,7 @@ ${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please Wait.')}`);
     conn.on('open', async () => {
         var st = Session.createStringSession(conn.base64EncodedAuthInfo());
         console.log(
-            chalk.green.bold('Queen Sew String: '), Session.createStringSession(conn.base64EncodedAuthInfo())
+            chalk.green.bold('Kingbot String: '), Session.createStringSession(conn.base64EncodedAuthInfo())
         );
         
         if (!fs.existsSync('config.env')) {
@@ -38,16 +38,16 @@ ${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please Wait.')}`);
         }
         if (conn.user.jid.startsWith('90')) {
             await conn.sendMessage(conn.user.jid,st, MessageType.text)
-            await conn.sendMessage(conn.user.jid,'*sew*', MessageType.text)
+            await conn.sendMessage(conn.user.jid,'*King*', MessageType.text)
             console.log(
-                chalk.blue.bold('sew.')
+                chalk.blue.bold('King.')
             );
         }
         else {
             await conn.sendMessage(conn.user.jid,st, MessageType.text)
             await conn.sendMessage(conn.user.jid,'*Do Not Share This Code With Anyone!*', MessageType.text)
             console.log(
-                chalk.blue.bold('If you are installing locale, you can start the bot with node sew.js')
+                chalk.blue.bold('If you are installing locale, you can start the bot with node King.js')
             );
         }
         
